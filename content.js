82f0346142dc580b380490c119e74e18bf970133
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2025-08-17 10:43:30 -0300

    Add first week

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
-const startDate = new Date("2025-09-01");
+const startDate = new Date("2025-08-17");
 const weeks = [
     {
         title: "Semana 1 – O Convite ao Labirinto",
         html: `
-            <p>“Não sei onde este caminho nos leva. Mas sei que, passo a passo, ele termina no mesmo lugar: no abraço que espero desde sempre.”</p>
-            <blockquote>“Não sei para onde vou, mas sei que não vou por aí.” — José Régio</blockquote>
+            <p class="fade-in">“Não sei onde este caminho nos leva. Mas sei que, passo a passo, ele termina no mesmo lugar: no abraço que espero desde sempre.”</p>
+            <blockquote class="fade-in delay-1">“Não sei para onde vou, mas sei que não vou por aí.” — José Régio</blockquote>
+            <img id="corvo" src="assets/img/corvo.png" alt="Corvo" class="pulse fade-in delay-2" onClick="corvoAlert()">
+            <p class="fade-in delay-2">Clique no corvo para receber sua primeira mensagem.</p>
         `
     },
+    // TODO: fill weeks 2 - 10
 ];
---
+++ weekFunctions.js
@@ -0,0 +1,3 @@
+function corvoAlert() {
+    alert('Seja bem-vinda ao labirinto que criei para nós. Um passo por semana… até o fim.');
+}
